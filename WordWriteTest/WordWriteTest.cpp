c1c8737e4674230137d77e5238ebb4d1383bc17f
root <root@v1641342.hosted-by-vdsina.ru>
2023-01-30 20:11:57 +0300

    first commit

---
+++ WordWriteTest/WordWriteTest.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <chrono>
+
+int main()
+{
+    std::cout << "Start typing the following sentence: 'I love code'\n";
+    auto start = std::chrono::steady_clock::now(); // time the start
+
+    std::string sentence; // String, typed from the keyboard
+    std::cin >> sentence; //enter
+
+    auto end = std::chrono::steady_clock::now(); // check end time
+
+    if (sentence == "I" || sentence == "love" || sentence == "code") { // If the word has already been shortened, shift start & end.
+        start = end;
+    }
+
+    auto elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start); // Calculate how many ms.
+
+    std::cout << "You made it: " << elapsed_ms.count() << " ms\n";
+
+    return 0;
+}
